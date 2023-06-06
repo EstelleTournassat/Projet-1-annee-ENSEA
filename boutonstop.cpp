69a1ca4f8beaf3718a28d4782b9b670c47827163
EstelleTournassat <130039216+EstelleTournassat@users.noreply.github.com>
2023-06-06 17:12:47 +0200

    Update boutonstop.cpp

--- boutonstop.cpp
+++ boutonstop.cpp
@@ -1,13 +1,14 @@
 /*
- * Code fait a partir de gettingstarted de la librairie nrf24
  * See documentation at https://nRF24.github.io/RF24
  * See License information at root directory of this library
+ * Author: Brendan Doherty (2bndy5)
  */
 
 /**
- * Transmission entre une carte receveur qui lit en continue et une Raspberry 
- * Transmetteur qui envoie une commande ou rien ne se passe et sinon la commande stop
- * Utilise ctrl +c pour quitter le programme
+ * A simple example of sending data from 1 nRF24L01 transceiver to another.
+ *
+ * This example was written to be used on 2 devices acting as "nodes".
+ * Use `ctrl+c` to quit at any time.
  */
 #include <ctime>       // time()
 #include <iostream>    // cin, cout, endl
@@ -19,8 +20,8 @@ using namespace std;
 
 /****************** Linux ***********************/
 // Radio CE Pin, CSN Pin, SPI Speed
-// CE Pin utilise le nombre GPIO avec BCM et les SPIDEV drivers
-// Les adresses CS Pin donne le nombre du bus SPI à /dev/spidev<a>.<b>
+// CE Pin uses GPIO number with BCM and SPIDEV drivers, other platforms use their own pin numbering
+// CS Pin addresses the SPI bus number at /dev/spidev<a>.<b>
 // ie: RF24 radio(<ce_pin>, <a>*10+<b>); spidev1.0 is 10, spidev1.1 is 11 etc..
 #define CSN_PIN 0
 #ifdef MRAA
@@ -30,26 +31,46 @@ using namespace std;
 #endif
 // Generic:
 RF24 radio(CE_PIN, CSN_PIN);
+/****************** Linux (BBB,x86,etc) ***********************/
+// See http://nRF24.github.io/RF24/pages.html for more information on usage
+// See http://iotdk.intel.com/docs/master/mraa/ for more information on MRAA
+// See https://www.kernel.org/doc/Documentation/spi/spidev for more information on SPIDEV
 
-int payload = 0.0;
-char* role;
-char* canal;
-int Canal=0;
-void master();  // prototype of the transmetteur
-void slave();   // prototype of the receveur
-void SetRole(); //prototype pour attribuer les rôles
+// For this example, we'll be using a payload containing
+// a single float number that will be incremented
+// on every successful transmission
+float payload = 0.0;
+
+void setRole(); // prototype to set the node's role
+void master();  // prototype of the TX node's behavior
+void slave();   // prototype of the RX node's behavior
 
 // custom defined timer for evaluating transmission time in microseconds
 struct timespec startTimer, endTimer;
+uint32_t getMicros(); // prototype to get elapsed time in microseconds
 
-int main(int argc, char* const argv[])
+int main(int argc, char** argv)
 {
 
-    // check l'installation de la radio
+    // perform hardware check
     if (!radio.begin()) {
-        cout << "La nfr24 ne répond pas!!" << endl;
+        cout << "radio hardware is not responding!!" << endl;
         return 0; // quit now
     }
+
+    // to use different addresses on a pair of radios, we need a variable to
+    // uniquely identify which address this radio will use to transmit
+    bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
+
+    // print example's name
+    cout << argv[0] << endl;
+
+    // Let these addresses be used for the pair
+    uint8_t address[2][6] = {"1Node", "2Node"};
+    // It is very helpful to think of an address as a path instead of as
+    // an identifying device destination
+
+    // Set the radioNumber and the role of the raspberry via the terminal on startup
     if (argc==1){}
     else if (argc==2){
         role=argv[1];}
@@ -61,97 +82,121 @@ int main(int argc, char* const argv[])
         Canal=0;
     }
     else{Canal=1;}
-
-    // pour utiliser différentes adresses sur une paire de radios, nous avons besoin d'une variable
-    // pour identifier de manière unique l'adresse que cette radio utilisera pour transmettre
-    bool radioNumber = 1; // 0 utilise l'adresse [0] pour transmettre, 1 utilise l'adresse [1] pour transmettre
-
-    // Laisser ces adresses être utilisées pour la paire
-    uint8_t address[2][6] = {"1Node", "2Node"};
-    // Il est très utile de considérer une adresse comme un chemin plutôt que 
-    //comme une destination de périphérique d'identification
-
-    // Définissez le radioNumber via le terminal au démarrage
     string Canal;
     radioNumber = Canal.length() > 0 && (uint8_t)Canal[0] == 49;
 
-    // économisez sur le temps de transmission en configurant la radio pour ne transmettre que 
-    // le nombre d'octets dont nous avons besoin pour transmettre un flottant
-    radio.setPayloadSize(sizeof(payload));
+    // save on transmission time by setting the radio to only transmit the
+    // number of bytes we need to transmit a float
+    radio.setPayloadSize(sizeof(payload)); // float datatype occupies 4 bytes
 
-    // Définissez le niveau PA bas pour essayer d'éviter les problèmes liés à l'alimentation, 
-    // car ces exemples sont probablement exécutés avec des nœuds à proximité les uns des autres
+    // Set the PA Level low to try preventing power supply related problems
+    // because these examples are likely run with nodes in close proximity to
+    // each other.
     radio.setPALevel(RF24_PA_LOW); // RF24_PA_MAX is default.
 
-    // définir l'adresse TX du nœud RX dans le canal TX
-    radio.openWritingPipe(address[radioNumber]); // utilise toujours le canal 0
+    // set the TX address of the RX node into the TX pipe
+    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
 
-    // définir l'adresse RX du nœud TX dans un canal RX
-    radio.openReadingPipe(1, address[!radioNumber]); // utile le canal 1
-    SetRole();
+    // set the RX address of the TX node into a RX pipe
+    radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
+
+    // For debugging info
+    // radio.printDetails();       // (smaller) function that prints raw register values
+    // radio.printPrettyDetails(); // (larger) function that prints human readable data
+
+    // ready to execute program now
+    setRole(); // calls master() or slave() based on user input
     return 0;
 }
 
-void SetRole(){
+/**
+ * set this node's role from stdin stream.
+ * this only considers the first char as input.
+ */
+void setRole()
+{
     if (role == std::string("R")){
         slave();
     }
-    else{
-        master();
-    }
-}
+    else{master();}
+} // setRole()
+
 /**
- * fonction du transmetteur
+ * make this node act as the transmitter
  */
 void master()
 {
-    radio.stopListening(); // met la radio en mode TX
-    string input = "";
-    cout << "*** Presser la touche 'S' pour stopper le robot\n";
-    cout << "*** Presser une autre touche pour ne rien faire\n";
-    getline(cin, input);
+    radio.stopListening(); // put radio in TX mode
 
+    string input = "";
+    cout << "*** Press 'S' to stop the robot. Press any other key to do nothing.\n";
     if (input[0] == 'S' || input[0] == 's'){
         payload=1.0;}
     else {payload=0.0;}
-    cout <<payload<<endl;
 
-    unsigned int failure = 0; // on regarde le nombre d'échec
-    while (failure < 20) {
-        bool report = radio.write(&payload, sizeof(float)); // transmettre & sauver le message
+    unsigned int failure = 0; // keep track of failures
+    while (failure < 6) {
+        clock_gettime(CLOCK_MONOTONIC_RAW, &startTimer);    // start the timer
+        bool report = radio.write(&payload, sizeof(float)); // transmit & save the report
+        uint32_t timerElapsed = getMicros();                // end the timer
 
         if (report) {
-            if (payload==1){ //message transmit
-                cout << "Commande stop activée"<< endl;
+            // payload was delivered
+            cout << "Transmission successful! Time to transmit = ";
+            cout << timerElapsed;                     // print the timer result
+            if (payload>0.5){
+                cout << " The robot stopp "<< endl; // print payload sent
             }
-            else {cout << "On ne fait rien"<< endl; }
-            
+            else {
+                cout << " Nothing happened "<< endl; // print payload sent
+            }
         }
         else {
-            // messaqge non transmis
-            cout << "Pas de transmission ou temps trop long " << endl;
+            // payload was not delivered
+            cout << "Transmission failed or timed out" << endl;
             failure++;
         }
 
-        delay(1000); // ralentit la transmission de 1 seconds
+        // to make this example readable in the terminal
+        delay(1000); // slow transmissions down by 1 second
     }
-    cout << failure << " Erreurs détectées. On arrête la transmission." << endl;
-} 
+    cout << failure << " failures detected. Leaving TX role." << endl;
+}
 
 /**
- * fonction du receveur
+ * make this node act as the receiver
  */
 void slave()
 {
-    while(1){
-        time_t startTimer = time(nullptr);       // start a timer
-        while (time(nullptr) - startTimer < 6) { // use 6 second timeout
-            uint8_t pipe;
-            if (radio.available(&pipe)) {                        // is there a payload? get the pipe number that recieved it
-                cout << payload << endl;                 		// print the payload's value
-                startTimer = time(nullptr);                      // reset timer
-            }
+
+    radio.startListening(); // put radio in RX mode
+
+    while (1) { 
+        uint8_t pipe;
+        if (radio.available(&pipe)) {                        // is there a payload? get the pipe number that recieved it
+            uint8_t bytes = radio.getPayloadSize();          // get the size of the payload
+            radio.read(&payload, bytes);                     // fetch payload from FIFO
+            cout << "Received " << (unsigned int)bytes;      // print the size of the payload
+            cout << " bytes on pipe " << (unsigned int)pipe; // print the pipe number
+            cout << ": " << payload << endl;                 // print the payload's value
         }
-        cout << "Nothing received in 6 seconds." << endl;
+    cout << "Nothing received." << endl
+    delay(1000);
     }
+    radio.stopListening();
+}
+
+/**
+ * Calculate the elapsed time in microseconds
+ */
+uint32_t getMicros()
+{
+    // this function assumes that the timer was started using
+    // `clock_gettime(CLOCK_MONOTONIC_RAW, &startTimer);`
+
+    clock_gettime(CLOCK_MONOTONIC_RAW, &endTimer);
+    uint32_t seconds = endTimer.tv_sec - startTimer.tv_sec;
+    uint32_t useconds = (endTimer.tv_nsec - startTimer.tv_nsec) / 1000;
+
+    return ((seconds)*1000 + useconds) + 0.5;
 }
